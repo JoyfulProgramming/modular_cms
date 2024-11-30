7bff2ee890b7c9c0c5c390a2f0baf13c1b1ab197
John Gallagher <john@synapticmishap.co.uk>
2024-11-30 14:07:40 +0000

    feat: introduce styled text in headings

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,6 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import MarkdownIt from "markdown-it";
+import DOMPurify from "isomorphic-dompurify";
 
 export default function (eleventyConfig) {
   eleventyConfig.ignores.add("./README.md");
@@ -8,6 +10,10 @@ export default function (eleventyConfig) {
   });
   eleventyConfig.setServerOptions({
     watch: ['_site/**/*.css'],
+  });
+  eleventyConfig.addFilter("markdown", function (content) {
+    const unsafeHtml = new MarkdownIt({ html: true }).render(content || "");
+    return DOMPurify.sanitize(unsafeHtml, { ALLOWED_TAGS: ['span', 'strong', 'em'] });
   });
 
   return {
